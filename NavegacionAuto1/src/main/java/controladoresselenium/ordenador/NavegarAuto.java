6ed023ab1946d46653f18e2e46c3564133abcd67
Carlos <carlos.andres@correo.aeat.es>
2021-06-23 22:48:19 +0100

    Navegacion

--- NavegacionAuto1/src/main/java/controladoresselenium/ordenador/NavegarAuto.java
+++ NavegacionAuto1/src/main/java/controladoresselenium/ordenador/NavegarAuto.java
@@ -2,6 +2,9 @@ package controladoresselenium.ordenador;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -33,7 +36,7 @@ public class NavegarAuto extends Thread {
 		String datoClaveFuera = (new Scanner(new File("C:\\DATOS\\navega1_NO_BORRAR\\clavefuera.txt"))).nextLine();
 		String datoClaveDentro = (new Scanner(new File("C:\\DATOS\\navega1_NO_BORRAR\\claveDentro.txt"))).nextLine();
 
-		System.out.println("Abriendo navegador IExplorer");
+		System.out.println("Configurando propiedades de navegador IExplorer...");
 		System.setProperty("webdriver.ie.driver", "C:\\WebDriver\\bin\\IEDriverServer.exe");
 
 		// OPCIONES DEL NAVEGADOR
@@ -49,7 +52,7 @@ public class NavegarAuto extends Thread {
 		ieo.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);// ACEPTAR CERTIFICADO!!
 
 		// -------------------------------------
-
+		System.out.println("Abriendo navegador IExplorer...");
 		WebDriver driver = new InternetExplorerDriver(ieo);// DRIVER
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Implicit wait: 10 segundos
 		driver.manage().window().maximize();// MAXIMIZAR ventana
@@ -64,20 +67,22 @@ public class NavegarAuto extends Thread {
 
 		sleep(1000L);
 
-		// -----------POP UP de Windows cetificado -----------------
-		Robot miRobot = new Robot(); // Actúa fuera de Selenium
+		// -----------POP UP de Windows certificado -----------------
+		System.out.println("POP UP de Windows certificado (robot 1)...");
+		Robot robot1 = new Robot(); // Actúa fuera de Selenium
 
 		// Tabulador
-		miRobot.keyPress(KeyEvent.VK_TAB);
-		miRobot.keyRelease(KeyEvent.VK_TAB);
+		robot1.keyPress(KeyEvent.VK_TAB);
+		robot1.keyRelease(KeyEvent.VK_TAB);
 
 		// Enter
-		miRobot.keyPress(KeyEvent.VK_ENTER);
-		miRobot.keyRelease(KeyEvent.VK_ENTER);
+		robot1.keyPress(KeyEvent.VK_ENTER);
+		robot1.keyRelease(KeyEvent.VK_ENTER);
 
 		sleep(3000L);
 
 		// ------- PANTALLA NEGRA---------------------
+		System.out.println("PANTALLA NEGRA...");
 
 		WebElement cajaUsuarioPantallaNegra = driver.findElement(By.id("Enter user name"));
 		WebElement cajaPasswdPantallaNegra = driver.findElement(By.id("passwd"));
@@ -89,6 +94,7 @@ public class NavegarAuto extends Thread {
 		botonPantallaNegra.click();
 
 		// ------- PANTALLA INTRANET---------------------
+		System.out.println("PANTALLA INTRANET...");
 		sleep(3000L);
 
 		// https://stackoverflow.com/questions/9588827/how-to-switch-to-the-new-browser-window-which-opens-after-click-on-the-button
@@ -109,7 +115,12 @@ public class NavegarAuto extends Thread {
 		botonPantallaIntranet.click();
 
 		// ----------- BOTONES GRANDES CON IMAGEN---------------
+		System.out.println("BOTONES GRANDES CON IMAGEN...");
 		sleep(1000L);
+
+		String handlePantallaBotonesGrandes = driver.getWindowHandle();
+		System.out.println("Guardamos el HANDLE de esta pantalla para volver luego: " + handlePantallaBotonesGrandes);
+
 		WebElement botonArribaTodasApps = driver.findElement(By.id("allAppsBtn"));
 		botonArribaTodasApps.click();
 		sleep(1000L);
@@ -121,47 +132,86 @@ public class NavegarAuto extends Thread {
 
 		// ------------ Abrir enlace (aviso alerta: "Abrir" o "Guardar Como")
 		// ---------------------------
+		System.out.println("ABRIENDO el fichero .ica que establecera la conexion...");
+
 		// Tabuladores
 		for (int i = 1; i <= 8; i++) {
-			miRobot.keyPress(KeyEvent.VK_TAB);
-			miRobot.keyRelease(KeyEvent.VK_TAB);
+			System.out.println("Tabulador numero " + i);
+			robot1.keyPress(KeyEvent.VK_TAB);
+			robot1.keyRelease(KeyEvent.VK_TAB);
 			sleep(1000L);
 		}
 
 		// Enter
-		miRobot.keyPress(KeyEvent.VK_ENTER);
-		miRobot.keyRelease(KeyEvent.VK_ENTER);
+		robot1.keyPress(KeyEvent.VK_ENTER);
+		robot1.keyRelease(KeyEvent.VK_ENTER);
+		Long esperaLarga = 40 * 1000L;
+		System.out.println("Esperando " + esperaLarga + " milisegundos...");
+		sleep(esperaLarga);
 
 		// -----------POP UP de Windows certificado -----------------
-		sleep(20 * 1000L);
+		System.out.println("POP UP de Windows certificado (pantalla 1 - Host remoto)...");
+
 		Robot miRobot2 = new Robot(); // Actúa fuera de Selenium
-		// Tabulador
-		miRobot2.keyPress(KeyEvent.VK_TAB);
-		miRobot2.keyRelease(KeyEvent.VK_TAB);
+
+		// 2 Tabuladores
+		for (int i = 1; i <= 2; i++) {
+			miRobot2.keyPress(KeyEvent.VK_TAB);
+			miRobot2.keyRelease(KeyEvent.VK_TAB);
+			sleep(1000L);
+		}
 
 		// Enter
 		miRobot2.keyPress(KeyEvent.VK_ENTER);
 		miRobot2.keyRelease(KeyEvent.VK_ENTER);
 
-		sleep(2000L);
+		sleep(4000L);
+
 		// --------------------------------------
+		System.out.println("POP UP de Windows certificado (pantalla 2 - Escribir clave)...");
 		Robot miRobot3 = new Robot(); // Actúa fuera de Selenium
 
-//		Alert alert = driver.switchTo().alert();
-//		alert.sendKeys(datoClaveDentro);
-//
-//		// 2 Tabuladores
-//		miRobot3.keyPress(KeyEvent.VK_TAB);
-//		miRobot3.keyRelease(KeyEvent.VK_TAB);
-//		sleep(500L);
-//		miRobot3.keyPress(KeyEvent.VK_TAB);
-//		miRobot3.keyRelease(KeyEvent.VK_TAB);
-//		sleep(500L);
-//
-//		// Enter
-//		miRobot3.keyPress(KeyEvent.VK_ENTER);
-//		miRobot3.keyRelease(KeyEvent.VK_ENTER);
+		StringSelection stringSelection = new StringSelection(datoClaveDentro); // CLAVE INTERNA
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clipboard.setContents(stringSelection, stringSelection);
 
+		// Pegar (Control-V)
+		Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+
+		// Enter
+		miRobot2.keyPress(KeyEvent.VK_ENTER);
+		miRobot2.keyRelease(KeyEvent.VK_ENTER);
+
+		sleep(4000L);
+
+		// --------------------------------------
+		// --------------------------------------
+		System.out.println(
+				"Volvemos a poner el foco en la pantalla de botones GRANDES y tabulamos hasta llegar a la Intranet de Movilidad (para fichar)...");
+
+		System.out.println("Usando este Handle: " + handlePantallaBotonesGrandes);
+		driver.switchTo().window(handlePantallaBotonesGrandes);
+
+		// 15 Tabuladores
+		System.out.println("Pulsamos muchos tabuladores hasta llegar al boton gordo deseado...");
+		for (int i = 1; i <= 15; i++) {
+			miRobot2.keyPress(KeyEvent.VK_TAB);
+			miRobot2.keyRelease(KeyEvent.VK_TAB);
+			sleep(1000L);
+		}
+
+		System.out.println("TEMPORAL Sleep gigante para hacerlo a mano...");
+		sleep(4000000L);
+
+		// Enter
+		miRobot2.keyPress(KeyEvent.VK_ENTER);
+		miRobot2.keyRelease(KeyEvent.VK_ENTER);
+
+		sleep(4000L);
 		// --------------------------------------
 
 		// Salir
@@ -169,4 +219,5 @@ public class NavegarAuto extends Thread {
 //		driver.quit();
 
 	}
+
 }
