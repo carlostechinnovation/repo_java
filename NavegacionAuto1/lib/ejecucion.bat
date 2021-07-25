f55cf4ee8620225ded6acfc5e53f7ce4597df0f8
Carlos <carlosandresgarcia1986@hotmail.com>
2021-07-25 22:42:56 +0200

    Cambios

--- NavegacionAuto1/lib/ejecucion.bat
+++ NavegacionAuto1/lib/ejecucion.bat
@@ -1,8 +1,8 @@
 @echo Directorio principal
 cd C:\DATOS\GITHUB_REPOS\repo_java\NavegacionAuto1\
 
-@echo Compilando el JAR con Maven (modo batch)
-call mvn clean compile assembly:single -B
+#@echo Compilando el JAR con Maven (modo batch)
+#call mvn clean compile assembly:single -B
 
 @echo Ejecutando la clase MAIN
 java -cp C:\DATOS\GITHUB_REPOS\repo_java\NavegacionAuto1\target\navegacionauto1-0.0.1-SNAPSHOT-jar-with-dependencies.jar controladoresselenium.ordenador.NavegarAuto
---
+++ youtubesimple/src/main/java/mytools/youtubesimple/App.java
@@ -0,0 +1,15 @@
+package mytools.youtubesimple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("YOUTUBE - SIMPLE: inicio");
+
+		System.out.println("YOUTUBE - SIMPLE: fin");
+
+	}
+}
